099f99497f09657374f8967ca5b973e1e47093ac
rupamghosh7777 <grick0273@gmail.com>
2024-06-13 21:52:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+console.log(saveEl)
+
+function increment() {
+    count = count + 1
+    countEl.textContent = count
+
+}
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent=0
+    count=0
+    console.log(count)
+}
